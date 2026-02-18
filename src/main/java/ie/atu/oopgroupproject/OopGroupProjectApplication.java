7696d0d66abb52de8f8a66df3c4b0d79ba54b1eb
EthanLourens-STUDENT <G00467054@atu.ie>
2026-02-18 17:12:12 +0000

    Start

---
+++ src/main/java/ie/atu/oopgroupproject/OopGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.oopgroupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopGroupProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopGroupProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oopgroupproject/OopGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oopgroupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopGroupProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
