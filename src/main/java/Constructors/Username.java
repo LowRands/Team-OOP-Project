c71deeadb2a3efe3cea06cd6dd96b88ad49affc9
EthanLourens-STUDENT <G00467054@atu.ie>
2026-02-18 18:03:11 +0000

    First

---
+++ src/main/java/Constructors/Username.java
@@ -0,0 +1,10 @@
+package Constructors;
+
+import ie.atu.oopgroupproject.Service.LoginService;
+
+public class Username{//} extends LoginService{
+   String email;
+//    public setUsername() {}
+    public String getEmail(){return email;
+    }
+}
---
+++ src/main/java/ie/atu/oopgroupproject/Service/LoginService.java
@@ -0,0 +1,13 @@
+package ie.atu.oopgroupproject.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    public String Username(String username) {
+            return  username;
+    }
+    public String Password;
+
+}
