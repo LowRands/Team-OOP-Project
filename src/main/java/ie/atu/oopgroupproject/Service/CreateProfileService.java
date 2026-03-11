a22242d638e24bc9d7cd4ac3fa883186c1a0a6cd
Finnja <g00470359@atu.ie>
2026-03-11 13:33:58 +0000

    added array list to store profiles and function to return all profiles

---
+++ src/main/java/ie/atu/oopgroupproject/Service/CreateProfileService.java
@@ -0,0 +1,28 @@
+package ie.atu.oopgroupproject.Service;
+
+import ie.atu.oopgroupproject.model.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class CreateProfileService {
+    private List<Profile> profiles = new ArrayList<Profile>();
+
+    public Profile createProfile(Profile profile) {
+        if(profile.getMedClearance() && !profile.getMedClearance()) {
+            throw new RuntimeException("Can't be an active swimmer without medical clearance")
+        }
+
+        profiles.add(profile);
+        return profile;
+    }
+
+    public List<Profile> getAllProfiles() {
+        return profiles;
+    }
+
+}
+
