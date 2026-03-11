0de82ac5083401ca62d6de17004e8a838e168946
Ethan <G00467054@atu.ie>
2026-03-11 13:54:10 +0000

    Start of checking if username exists

--- src/main/java/ie/atu/oopgroupproject/Controller/LoginScreenControl.java
+++ src/main/java/ie/atu/oopgroupproject/Controller/LoginScreenControl.java
@@ -1,36 +1,35 @@
 package ie.atu.oopgroupproject.Controller;
 
-import ie.atu.oopgroupproject.Constructors.Username;
 import ie.atu.oopgroupproject.Service.LoginService;
+import ie.atu.oopgroupproject.model.Login;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 
 @RestController
-@RequestMapping
-
-
+@RequestMapping("/Login")
 public class LoginScreenControl {
     public static void main(String[] args) {
     }
 
-    ArrayList<Username> nameList = new ArrayList<>();
+   // ArrayList<Username> nameList = new ArrayList<>();
     private final LoginService Login;
 
     public LoginScreenControl(LoginService LoginService) {
         this.Login = LoginService;
     }
+    @GetMapping("/{Username}")
+    public ResponseEntity<Login> getUsername(@PathVariable String Username){
+        return ResponseEntity.ok(LoginService.getUsername(username));
+    }
 
     @GetMapping("/Login")
     public ResponseEntity<String> Username(@RequestParam String Username, String Email, String Password) {
-        String user = Login.Username(Username, Email, Password);
+        //String user = Login.Username(Username, Email, Password);
         String fileName = "Users";
         try(PrintWriter out = new PrintWriter(new FileWriter(fileName, true))) {
             out.println("Username: " + Username);
--- src/main/java/ie/atu/oopgroupproject/Service/LoginService.java
+++ src/main/java/ie/atu/oopgroupproject/Service/LoginService.java
@@ -1,14 +1,33 @@
 package ie.atu.oopgroupproject.Service;
 
 
+import ie.atu.oopgroupproject.Exception.LoginConflict;
+import ie.atu.oopgroupproject.model.Login;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoginService {
     public String Username(String username, String email, String password) {
-            return  username;
+        return username;
     }
+
     public String Password;
     public String Email;
+
+    private final List<Login> Logins = new ArrayList<>();
+
+    boolean UserExists = Logins.stream()
+            .anyMatch(b -> b.getUsername().equals(Login.getUsername()));
+
+    public Login getUsername(String username) {
+        return Logins.stream()
+                .filter(b -> b.getUsername().equals(username))
+                .findFirst()
+                .orElseThrow(() -> new LoginConflict("Login not found: " + username));
+    }
+
 
 }
