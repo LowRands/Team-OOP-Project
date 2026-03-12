dde44e95d283721c5aa5535ceb6e80ff92abbf2a
EthanLourens-STUDENT <G00467054@atu.ie>
2026-03-12 10:31:09 +0000

    Checks if information added to the array matches Users input information

--- src/main/java/ie/atu/oopgroupproject/Controller/LoginScreenControl.java
+++ src/main/java/ie/atu/oopgroupproject/Controller/LoginScreenControl.java
@@ -19,33 +19,15 @@ public class LoginScreenControl {
         List<String> Usernames = new ArrayList<>();
         List<String> Passwords = new ArrayList<>();
         List<String> Emails = new ArrayList<>();
-        if (Username == null || Username.isEmpty() || Email == null || Email.isEmpty() || Password == null || Password.isEmpty()) {
-            response.put("Username:", "Invalid Username");
-        } else {
-            Usernames.add(Username);
-            if (Usernames.contains(Username)) {
-                response.put("Username:", "Valid Username");
+        Usernames.add("Ethan");
+        Passwords.add("123");
+        Emails.add("Ethan@gmail.com");
+        if (Username == null || Username.isEmpty() || Email == null || Email.isEmpty() && Email.contains("@") && Email.contains(".") || Password == null || Password.isEmpty() || Password != null) {
+            if (Usernames.contains(Username) && Emails.contains(Email) && Passwords.contains(Password)) {
+                response.put("Status:", "Login Successful");
+            } else {
+                response.put("Status:", "Login Failed. Incorrect Username, Email or Password");
             }
-        }
-        if (Email != null && Email.contains("@") && Email.contains(".")) {
-            Emails.add(Email);
-            if (Emails.contains(Email)) {
-                response.put("Email:", "Valid Email");
-            }
-        } else {
-            response.put("Email:", "Invalid Email");
-        }
-        if (Password != null) {
-            Passwords.add(Password);
-            if (Passwords.contains(Password)) {
-                response.put("Password:", "Valid Password");
-            }
-        }
-        Logins.add(Usernames);
-        Logins.add(Emails);
-        Logins.add(Passwords);
-        if(Logins.contains(Usernames) && Logins.contains(Emails) && Logins.contains(Passwords)) {
-            System.out.println("Login Success");
         }
         return response;
     }
