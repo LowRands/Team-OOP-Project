8f2913c8840224cd70185a6d1df22752364c349b
Ethan <G00467054@atu.ie>
2026-03-16 13:57:52 +0000

    Start of checking if username exists

--- src/main/java/ie/atu/oopgroupproject/Service/LoginService.java
+++ src/main/java/ie/atu/oopgroupproject/Service/LoginService.java
@@ -1,7 +1,31 @@
 package ie.atu.oopgroupproject.Service;
 
+import ie.atu.oopgroupproject.model.Login;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LoginService {
+    private List<Login> users = new ArrayList<>();
+
+    public String validateUser(Login login){
+
+        for(Login user : users){
+
+            if(user.getUsername().equals(login.getUsername()) &&
+                    user.getPassword().equals(login.getPassword()) && user.getEmail().equals(login.getEmail())){
+
+                return "Login Successful";
+            }
+        }
+
+        return "Login Failed";
+    }
+
+    public void addUser(Login login){
+        users.add(login);
+    }
+
 }
--- src/main/java/ie/atu/oopgroupproject/model/Login.java
+++ src/main/java/ie/atu/oopgroupproject/model/Login.java
@@ -5,18 +5,56 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
-@Getter
-public class Login {
-        @NotNull(message = "Username Can't be empty")
-        private String Username;
+//@Getter
+public class Login{
 
-        @NotBlank(message = "Email is required")
-        @Email(message = "Invalid email format")
-        private String Email;
+    private String username;
+    private String email;
+    private String password;
 
-        public Login(){}
+    public Login() {}
 
-    public void setUsername(String roomNumber) { this.Username = Username; }
+    public Login(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
-    public void setEmail(String studentEmail) { this.Email = Email; }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+//public class Login {
+//        @NotNull(message = "Username Can't be empty")
+//        private String Username;
+//
+//        @NotBlank(message = "Email is required")
+//        @Email(message = "Invalid email format")
+//        private String Email;
+//
+//        public Login(){}
+//
+//    public void setUsername(String roomNumber) { this.Username = Username; }
+//
+//    public void setEmail(String studentEmail) { this.Email = Email; }
+//}
