12d89b2dd32c7643d1401b7e9c47cd54173f482d
FinnjaBayer <g00470359@atu.ie>
2026-03-18 17:03:09 +0000

    added swimmer profile repository

---
+++ src/main/java/ie/atu/oopgroupproject/Repository/SwimmerProfileRepo.java
@@ -0,0 +1,8 @@
+package ie.atu.oopgroupproject.Repository;
+
+import ie.atu.oopgroupproject.model.SwimmerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SwimmerProfileRepo extends JpaRepository<SwimmerProfile,Integer> {
+
+}
